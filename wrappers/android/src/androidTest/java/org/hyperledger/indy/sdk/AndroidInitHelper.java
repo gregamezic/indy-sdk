4e8e35a603f496cbdfecea8d6202016051bb4493
Blaz Solar <me@blaz.solar>
2020-10-06 14:24:27 +0200

    Test pathds

--- wrappers/android/src/androidTest/java/org/hyperledger/indy/sdk/AndroidInitHelper.java
+++ wrappers/android/src/androidTest/java/org/hyperledger/indy/sdk/AndroidInitHelper.java
@@ -9,45 +9,42 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
+import androidx.test.platform.app.InstrumentationRegistry;
 import pl.brightinventions.slf4android.LogLevel;
 import pl.brightinventions.slf4android.LoggerConfiguration;
 
 public class AndroidInitHelper {
 
     public static void init() throws ErrnoException {
-//        StrictMode.ThreadPolicy policy = new
-//                StrictMode.ThreadPolicy.Builder().permitAll().build();
-//        StrictMode.setThreadPolicy(policy);
-
         Logger logger = LoggerFactory.getLogger("org.hyperledger.indy.sdk.LibIndy.native");
 
         LoggerConfiguration.configuration()
                 .setLogLevel("org.hyperledger.indy.sdk.LibIndy.native", LogLevel.TRACE);
 
-        File cache = new File(EnvironmentUtils.getIndyHomePath());
+        File walletDir = new File(InstrumentationRegistry.getInstrumentation().getContext().getCacheDir().getAbsolutePath() + "/wallet/");
         File tmp = new File(EnvironmentUtils.getTmpPath());
-
-        cache.delete();
-        tmp.delete();
-
-        if (!cache.exists()) {
-            if (!cache.mkdirs()) {
-                throw new IllegalArgumentException("Cache dir fail.");
-            }
-        } else if (!cache.isDirectory()) {
-            throw new IllegalArgumentException("Cache not a dir.");
-        }
-
-        if (!tmp.exists()) {
-            if (!tmp.mkdirs()) {
-                throw new IllegalArgumentException("Tmp dir fail.");
-            }
-        } else if (!tmp.isDirectory()) {
-            throw new IllegalArgumentException("Tmp not a dir.");
-        }
+//
+//        cache.delete();
+//        tmp.delete();
+//
+//        if (!cache.exists()) {
+//            if (!cache.mkdirs()) {
+//                throw new IllegalArgumentException("Cache dir fail.");
+//            }
+//        } else if (!cache.isDirectory()) {
+//            throw new IllegalArgumentException("Cache not a dir.");
+//        }
+//
+//        if (!tmp.exists()) {
+//            if (!tmp.mkdirs()) {
+//                throw new IllegalArgumentException("Tmp dir fail.");
+//            }
+//        } else if (!tmp.isDirectory()) {
+//            throw new IllegalArgumentException("Tmp not a dir.");
+//        }
 
 //        if (!cacheDir.equals("")) {
-            Os.setenv("EXTERNAL_STORAGE", cache.getAbsolutePath(), true);
+            Os.setenv("EXTERNAL_STORAGE", walletDir.getAbsolutePath(), true);
             Os.setenv("TMPDIR", tmp.getAbsolutePath(), true);
 
             if (!LibIndy.isInitialized()) {
--- wrappers/android/src/androidTest/java/org/hyperledger/indy/sdk/utils/StorageUtils.java
+++ wrappers/android/src/androidTest/java/org/hyperledger/indy/sdk/utils/StorageUtils.java
@@ -1,13 +1,9 @@
 package org.hyperledger.indy.sdk.utils;
 
-import android.content.Context;
-
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
-
-import androidx.test.platform.app.InstrumentationRegistry;
 
 
 public class StorageUtils {
@@ -25,9 +21,6 @@ public class StorageUtils {
 
 		StorageUtils.cleanDirectory(tmpDir);
 		StorageUtils.cleanDirectory(homeDir);
-
-		Context instrumentationContext = InstrumentationRegistry.getInstrumentation().getContext();
-		StorageUtils.cleanDirectory(instrumentationContext.getCacheDir().getAbsoluteFile());
 	}
 
 }
