078ef46b240a34ec93c79dfff05b3f5eea2fce3d
Blaz Solar <me@blaz.solar>
2020-10-12 17:35:54 +0200

    Cleanup

--- wrappers/android/src/androidTest/java/org/hyperledger/indy/sdk/AndroidInitHelper.java
+++ wrappers/android/src/androidTest/java/org/hyperledger/indy/sdk/AndroidInitHelper.java
@@ -7,13 +7,9 @@ import android.util.Log;
 import com.sun.jna.Callback;
 import com.sun.jna.Pointer;
 
-import org.apache.commons.io.FileUtils;
 import org.hyperledger.indy.sdk.utils.EnvironmentUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.io.IOException;
 
 import pl.brightinventions.slf4android.LogLevel;
 import pl.brightinventions.slf4android.LoggerConfiguration;
@@ -28,53 +24,24 @@ public class AndroidInitHelper {
         LoggerConfiguration.configuration()
                 .setLogLevel("org.hyperledger.indy.sdk.LibIndy.native", LogLevel.TRACE);
 
-//        File walletDir = new File(InstrumentationRegistry.getInstrumentation().getContext().getCacheDir().getAbsolutePath() + "/wallet/");
-        File tmp = new File(EnvironmentUtils.getTmpPath());
-//
-//        cache.delete();
-        tmp.delete();
-        try {
-            FileUtils.forceMkdirParent(tmp);
-        } catch (IOException e) {
-            throw new IllegalArgumentException("Tmp dir fail.");
+        // TODO should this be set as part of init?
+        Os.setenv("EXTERNAL_STORAGE", EnvironmentUtils.getIndyHomePath(), true);
+        Os.setenv("TMPDIR", EnvironmentUtils.getTmpPath(), true);
+
+        if (!LibIndy.isInitialized()) {
+            LibIndy.init();
         }
 
-//        if (!cache.exists()) {
-//            if (!cache.mkdirs()) {
-//                throw new IllegalArgumentException("Cache dir fail.");
-//            }
-//        } else if (!cache.isDirectory()) {
-//            throw new IllegalArgumentException("Cache not a dir.");
-//        }
-//
-//        if (!tmp.exists()) {
-//            if (!tmp.mkdirs()) {
-//                throw new IllegalArgumentException("Tmp dir fail.");
-//            }
-//        } else if (!tmp.isDirectory()) {
-//            throw new IllegalArgumentException("Tmp not a dir.");
-//        }
-
-//        if (!cacheDir.equals("")) {
-            Os.setenv("EXTERNAL_STORAGE", EnvironmentUtils.getIndyHomePath(), true);
-            Os.setenv("TMPDIR", EnvironmentUtils.getTmpPath(), true);
-            Os.setenv("TMP", EnvironmentUtils.getTmpPath(), true);
-
-            if (!LibIndy.isInitialized()) {
-                LibIndy.init();
-            }
-
-            LibIndy.api.indy_set_logger(null, null, Test.call, null);
-//            LibIndy.api.indy_set_log_max_lvl(5);
-//        } else {
-//            throw new IllegalArgumentException("External storage path must be provided.");
-//        }
+        // TODO remove
+        LibIndy.api.indy_set_logger(null, null, Test.call, null);
     }
 
+    // TODO remove
     static class Test {
         private static Callback call = new Callback() {
 
-            public void callback(Pointer context, int level, String target, String message, String module_path, String file, int line) {
+            public void callback(Pointer context, int level, String target, String message,
+                    String module_path, String file, int line) {
                 detach(false);
 
                 Log.v("Indy log", message);
